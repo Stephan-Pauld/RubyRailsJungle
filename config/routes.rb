ea1279753b9ff269e8c182c9766be4c9f519ec8d
Stephan Paul <stephanpauld@gmail.com>
2021-01-06 14:56:31 -0700

    Add user authentications/login

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,21 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
-  get 'about' => 'about#show'
-
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
+    resources :sales, only: [:index, :new]
   end
+
+  get 'about' => 'about#show'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -75,4 +82,16 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
+
+
+
+
+
+
+
+
 end
+
+
