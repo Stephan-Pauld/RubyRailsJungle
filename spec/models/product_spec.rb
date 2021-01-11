cefc2eafaac4149466b1778c44f24e411b09a168
Stephan Paul <stephanpauld@gmail.com>
2021-01-11 16:55:14 -0700

    Add testing for creating categories

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,45 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   
   describe 'Validations' do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    it 'Should Have a Name' do
+      @category = Category.new(name: "Test")
+      @category.save!
+      @product = Product.new(name: 'name', price: 1000, quantity: 30, category: @category)
+      @product.save!
+      expect(@product.id).to be_present
+    end
 
+    it 'Should Fail On Save For Nil Name' do
+      @category = Category.new(name: "Test")
+      @category.save!
+      @product = Product.new(name: nil, price: 1000, quantity: 30, category: @category)
+      expect{@product.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'Should Fail On Save For Nil price' do
+      @category = Category.new(name: "Test")
+      @category.save!
+      @product = Product.new(name: nil, price: nil, quantity: 30, category: @category)
+      expect{@product.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'Should Fail On Save For Nil Quantity' do
+      @category = Category.new(name: "Test")
+      @category.save!
+      @product = Product.new(name: nil, price: 1000, quantity: nil, category: @category)
+      expect{@product.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'Should Fail On Save For Nil Category' do
+      @product = Product.new(name: nil, price: 1000, quantity: 30, category: nil)
+      expect{@product.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'Should Fail On Save For Nil Name' do
+      @category = Category.new(name: "Test")
+      @category.save!
+      @product = Product.new(name: nil, price: 1000, quantity: 30, category: @category)
+      expect{@product.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "Should have password and confirmed Password" do
+    
+  end
+
+
+
+
+
 end
